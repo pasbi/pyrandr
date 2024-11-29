e6a784ad383ebd1a528f1e88306c2d221568c557
pasbi <pasbi@users.noreply.github.com>
2024-11-29 13:32:17 +0100

    Create python-app.yml

--- test/test_parsing.py
+++ test/test_parsing.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
-
-import sys
-sys.path.append("..")
-import pyrandr
 import pytest
 
+import pathlib
+test_root =pathlib.Path(__file__).parent.resolve()
+
+import sys
+sys.path.append(str(test_root.parent))
+
+import pyrandr
 
 filenames = ["a.txt", "b.txt"]
+filenames = [
+    test_root / "test-cases" / filename
+    for filename in filenames
+]
 
 
 @pytest.mark.parametrize("filename", filenames)
 def test_parse(filename):
-    with open("test-cases/" + filename) as f:
+    with open(filename) as f:
         text = f.read()
     screens = list(pyrandr.parse_screens(text.splitlines()))
     assert len(screens) > 0
